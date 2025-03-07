4468bed7964e2b57ca224aa3b40c45c23b24c375
Kuba <kuba.krol.ldz@gmail.com>
2025-03-07 18:19:55 +0100

    Ogarniecie dynamicznie pages

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -15,7 +15,20 @@ export default function Footer() {
   return (
     <footer className="container max-w-7xl mx-auto gap-6 justify-center items-center p-4 xl:px-0 pt-0">
       <div className="sm:flex hidden flex-col gap-12">
-        {loading ? null : error ? null : (
+        {loading ? (
+          <div className="grid sm:grid-cols-5 gap-6 lg:grid-cols-5">
+            {[...Array(5)].map((_, index) => (
+              <div key={index} className="animate-pulse">
+                <div className="h-4 w-24 bg-gray-300 rounded mb-2" />
+                <ul className="space-y-2">
+                  {[...Array(3)].map((_, i) => (
+                    <li key={i} className="h-3 w-20 bg-gray-200 rounded" />
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        ) : error ? null : (
           <div className={`grid sm:grid-cols-5 gap-6 lg:grid-cols-5`}>
             {mainMenuItems.map((item) => (
               <div key={item.node.label}>
@@ -61,15 +74,15 @@ export default function Footer() {
       <div className="flex sm:hidden flex flex-col gap-6">
         <PTE_LOGO_FOOTER />
         <p className="text-end text-sm">
-            © Polskie Towarzystwo Ekonomiczne - Wszelkie prawa zastrzeżone |{" "}
-            <Link
-              href={"https://pte.pl/aktualnosci/polityka-prywatnosci"}
-              className="text-[#17822e] font-medium"
-              aria-label="politykaPrywatności"
-            >
-              Polityka prywatności
-            </Link>
-          </p>
+          © Polskie Towarzystwo Ekonomiczne - Wszelkie prawa zastrzeżone |{" "}
+          <Link
+            href={"https://pte.pl/aktualnosci/polityka-prywatnosci"}
+            className="text-[#17822e] font-medium"
+            aria-label="politykaPrywatności"
+          >
+            Polityka prywatności
+          </Link>
+        </p>
       </div>
     </footer>
   );
